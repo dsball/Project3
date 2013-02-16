2743b975e6999731b162205d5f3a22e3e91cbb24
D.S.Ball <wolfwoodster@gmail.com>
2013-02-16 12:56:08 -0700

    Added stack.cpp

---
+++ LinkedStructures/LinkedStructures/stack.cpp
@@ -0,0 +1,27 @@
+#include "stack.h"
+
+
+Stack::Stack()
+{
+	head = new node;
+}
+
+Stack Stack::Push()
+{
+
+}
+
+Stack Stack::Pop()
+{
+
+}
+
+void Stack::List()
+{
+
+}
+
+Stack::~Stack()
+{
+
+}
